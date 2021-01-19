bc77d2716c62db8e411d1d30e2b8957e6f3cfd08
talesb <talesbatistafig@hotmail.com>
2021-01-18 23:10:35 -0300

    Add home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from './home';
 import Navbar from './navbar';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <>
+      <Navbar/>
+      <Home/>
+    </>
   );
 }
 
--- front-web/src/navbar/index.tsx
+++ front-web/src/navbar/index.tsx
@@ -1,5 +1,5 @@
-import './styles.css';
 import { ReactComponent as Logo } from './logo.svg';
+import './styles.css';
 
 function Navbar() {
 
