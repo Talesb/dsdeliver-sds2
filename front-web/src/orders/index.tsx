3c5fcc72b857e5dc8fce8759c5aaeddd1d65a1a2
Talesb <talesbatistafig@hotmail.com>
2021-01-31 01:35:59 -0300

    Search order location

--- front-web/src/orders/index.tsx
+++ front-web/src/orders/index.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from 'react';
 import { fetchProducts } from '../api';
+import OrderLocation from './OrderLocation';
 import ProductsList from './ProductsList';
 import StepsHeaders from './StepsHeaders';
 import './styles.css';
-import { Product } from './types';
+import { OrderLocationData, Product } from './types';
 
 function Orders() {
 
 
     const [products, setProducts] = useState<Product[]>([]);
+    const [orderLocation,setOrderLocation]= useState<OrderLocationData>();
 
 
     console.log(products);
@@ -23,6 +25,7 @@ function Orders() {
         <div>
             <StepsHeaders />
             <ProductsList products={products} />
+            <OrderLocation  onChangeLocation={location => setOrderLocation(location)}/>
         </div>
     )
 
